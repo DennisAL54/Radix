e8d3664f9a3905dc16a60dae2e31a95fceb2b903
Unknown <alejimenezc@hotmail.com>
2019-10-07 12:21:34 -0600

    Inicio

---
+++ src/Radix.java
@@ -0,0 +1,22 @@
+public class Radix {
+
+    public void Ordenar(int[] Lista){
+        int n;
+        n = Lista.length;
+        int i;
+        int c;
+        c = 0;
+        int[] ListaRef;
+        ListaRef = Lista;
+        for(i=c; i<= n; i++){
+            if(Lista[i]%10 == Lista[i]){
+                Lista[i] = Lista[c];
+
+            }
+            else{
+
+            }
+        }
+
+    }
+}
