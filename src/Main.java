b3707c6c3d05bb87c44b72a02da3b100b1ed91d9
Unknown <alejimenezc@hotmail.com>
2019-10-08 23:15:06 -0600

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] Args){
+        int[] Arr = {200,35,46,84,100,20,2};
+        Radix RADIX = new Radix();
+        RADIX.Ordenar(Arr);
+    }
 }
--- src/Radix.java
+++ src/Radix.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Radix {
 
     public void Ordenar(int[] Lista){
@@ -6,17 +7,69 @@ public class Radix {
         int i;
         int c;
         c = 0;
-        int[] ListaRef;
-        ListaRef = Lista;
-        for(i=c; i<= n; i++){
-            if(Lista[i]%10 == Lista[i]){
-                Lista[i] = Lista[c];
+        int d;
+        d = 10;
+        ArrayList<Integer> Lista0 = new ArrayList<>();
+        ArrayList<Integer> Lista1 = new ArrayList<>();
+        ArrayList<Integer> Lista2 = new ArrayList<>();
+        ArrayList<Integer> Lista3 = new ArrayList<>();
+        ArrayList<Integer> Lista4 = new ArrayList<>();
+        ArrayList<Integer> Lista5 = new ArrayList<>();
+        ArrayList<Integer> Lista6 = new ArrayList<>();
+        ArrayList<Integer> Lista7 = new ArrayList<>();
+        ArrayList<Integer> Lista8 = new ArrayList<>();
+        ArrayList<Integer> Lista9 = new ArrayList<>();
+        while(c!=n){
+            for(i=c; i<= n; i++){
+                if(Lista[i]%d == Lista[i]){
+                    Lista[i] = Lista[c];
+                    Lista[c] = Lista[i];
+                    c = c+1;
+                }
+                else if(Lista[i]%d == 0){
+                    Lista0.add(Lista[i]);
 
-            }
-            else{
+                }
+                else if(Lista[i]%d == 1){
+                    Lista1.add(Lista[i]);
 
+                }
+                else if(Lista[i]%d == 2){
+                    Lista2.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 3){
+                    Lista3.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 4){
+                    Lista4.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 5){
+                    Lista5.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 6){
+                    Lista6.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 7){
+                    Lista7.add(Lista[i]);
+
+                }
+                else if(Lista[i]%d == 8){
+                    Lista8.add(Lista[i]);
+
+                }
+                else{
+                    Lista9.add(Lista[i]);
+
+                }
             }
+
         }
+
 
     }
 }
